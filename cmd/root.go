e9f7dcfb4c07bd48a40174dcb148dbe08783face
diashelter <helter.dias@gmail.com>
2023-12-11 17:51:11 -0300

    ïnitial commit

---
+++ cmd/root.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/diashelter/go-hexagonal/application"
+	"os"
+	"github.com/spf13/cobra"
+	dbInfra "github.com/diashelter/go-hexagonal/adapters/db"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var db, _ = sql.Open("sqlite3","db.sqlite")
+var productDb = dbInfra.NewProductDb(db)
+var productService = application.ProductService{Persistence: productDb}
+
+var rootCmd = &cobra.Command{
+	Use:   "go-hexagonal",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-hexagonal.yaml)")
+
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".go-hexagonal")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
